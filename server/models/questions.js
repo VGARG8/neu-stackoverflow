20e3dbee293762368d79f47dff2a08f1304d9ac3
knyven <vknyaz06@gmail.com>
2023-11-12 19:53:22 -0500

    added models

--- server/models/questions.js
+++ server/models/questions.js
@@ -1,1 +1,16 @@
 // Question Document Schema
+
+
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }],
+  asked_by: { type: String, required: true },
+  ask_date_time: { type: Date, required: true },
+  views: { type: Number, default: 0 },
+  answers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Answer' }]
+});
+
+module.exports = mongoose.model('Question', questionSchema);
