16886e2c1fd5925a96bed7230f3e27b117497bfa
knyven <vknyaz06@gmail.com>
2023-12-07 23:15:39 -0500

    buttons for comment voting not functional..yet

---
+++ server/controllers/commentVote.js
@@ -0,0 +1,51 @@
+const Comment = require("../models/comments");
+
+exports.postUpvoteComment = async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const comment = await Comment.findById(commentId).populate("commented_by");
+
+    if (!comment) return res.status(404).send("Comment not found");
+
+    // Increment votes by one for an upvote
+    comment.votes += 1;
+    await comment.save();
+
+    // Update user's reputation
+    if (comment.commented_by) {
+      // Increment user's reputation
+      comment.commented_by.reputation += 1;
+      await comment.commented_by.save();
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    console.error("Error upvoting comment:", error);
+    res.status(500).send(error);
+  }
+};
+
+exports.postDownvoteComment = async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const comment = await Comment.findById(commentId).populate("commented_by");
+
+    if (!comment) return res.status(404).send("Comment not found");
+
+    // Decrement votes by one for a downvote
+    comment.votes -= 1;
+    await comment.save();
+
+    // Update user's reputation
+    if (comment.commented_by) {
+      // Decrement user's reputation
+      comment.commented_by.reputation -= 1;
+      await comment.commented_by.save();
+    }
+
+    res.status(200).json(comment);
+  } catch (error) {
+    console.error("Error downvoting comment:", error);
+    res.status(500).send(error);
+  }
+};
